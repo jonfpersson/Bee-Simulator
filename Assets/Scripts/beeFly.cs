70ab4cf6233cf4e955f822141c04de5b43aac976
Jonathan Persson <jonfpersson@gmail.com>
2018-05-21 21:36:29 +0200

    fixed bugs and added flying

--- Assets/Scripts/beeFly.cs
+++ Assets/Scripts/beeFly.cs
@@ -2,37 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class beeFly : MonoBehaviour {
+public class beeFly : MonoBehaviour
+{
+    /*
+EXTENDED FLYCAM
+    Desi Quintans (CowfaceGames.com), 17 August 2012.
+    Based on FlyThrough.js by Slin (http://wiki.unity3d.com/index.php/FlyThrough), 17 May 2011.
 
-    public float speed = 6.0F;
-    public float gravity = 20.0F;
+LICENSE
+    Free as in speech, and free as in beer.
 
-    private Vector3 moveDirection = Vector3.zero;
-    public CharacterController controller;
+FEATURES
+    WASD/Arrows:    Movement
+              Q:    Climb
+              E:    Drop
+                  Shift:    Move faster
+                Control:    Move slower
+                    End:    Toggle cursor locking to screen (you can also press Ctrl+P to toggle play mode on and off).
+*/
+
+    public float cameraSensitivity = 90;
+    public float climbSpeed = 4;
+    public float normalMoveSpeed = 10;
+    public float slowMoveFactor = 0.25f;
+    public float fastMoveFactor = 3;
+
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
 
     void Start()
     {
-        // Store reference to attached component
-        controller = GetComponent<CharacterController>();
+        Screen.lockCursor = true;
     }
 
     void Update()
     {
-        // Character is on ground (built-in functionality of Character Controller)
-        if (controller.isGrounded)
+        rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+        rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
 
+        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
 
-
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
-            // Use input up and down for direction, multiplied by speed
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-
+            transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
         }
-        // Apply gravity manually.
-        moveDirection.y -= gravity * Time.deltaTime;
-        // Move Character Controller
-        controller.Move(moveDirection * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+            transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        }
+
+
+        if (Input.GetKey(KeyCode.Q)) { transform.position += transform.up * climbSpeed * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.E)) { transform.position -= transform.up * climbSpeed * Time.deltaTime; }
+
+        if (Input.GetKeyDown(KeyCode.End))
+        {
+            Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
+        }
     }
+
 }
---
+++ Assets/Scripts/beeWingAnimator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beeWingAnimator : MonoBehaviour {
+
+    Animator wingAnimator;
+
+
+    // Use this for initialization
+    void Start () {
+        wingAnimator = gameObject.GetComponent<Animator>();
+        wingAnimator.Play("play");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
