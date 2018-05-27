e99307b87ec9957edd145605b67c017505e4ffbe
Jonathan Persson <jonfpersson@gmail.com>
2018-05-27 03:10:28 +0200

    Added entering to bee hive and performance fixes

---
+++ Assets/Scripts/enterBeeHive.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enterBeeHive : MonoBehaviour {
+
+    public GameObject hive;
+    public GameObject enterHiveMsg;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector3.Distance(followPointCoordinate, transform.position)
+
+        if(Vector3.Distance(hive.transform.position, transform.position) < 5)
+        {
+            enterHiveMsg.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                StartCoroutine(enterHive());
+                enterHiveMsg.SetActive(false);
+
+            }
+
+        } else
+            enterHiveMsg.SetActive(false);
+
+        //Debug.Log("HIVE: " + hive.transform.position);
+        //Debug.Log("player: " + transform.position);
+
+    }
+
+    IEnumerator enterHive()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("beeHouse");
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+}
---
+++ Assets/Scripts/pickUpNectar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpNectar : MonoBehaviour {
+
+    float maxDistance = 10;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        // Will contain the information of which object the raycast hit
+        RaycastHit hit;
+        
+        float thickness = 1f; //<-- Desired thickness here.
+        Vector3 origin = transform.position + new Vector3(0, 0.6f, -1.6f);
+        Vector3 direction = transform.TransformDirection(Vector3.forward);
+                    
+        if (Physics.SphereCast(origin, thickness, direction, out hit) && hit.collider.name == "flower")
+        {
+            Debug.Log("hit");
+
+
+        }
+      //  Debug.Log(hit.collider.name);
+
+
+
+    }
+
+}
