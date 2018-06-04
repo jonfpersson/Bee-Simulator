fe34fc794a6c4b865b27f67ba9397b3e8db6e79e
Jonathan Persson <jonfpersson@gmail.com>
2018-06-04 21:16:46 +0200

    Started on rotation

--- Assets/Scripts/rotateIcons.cs
+++ Assets/Scripts/rotateIcons.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class rotateIcons : MonoBehaviour {
 
-	
+    public GameObject[] pictures;
     public void rotatePictures(string direction)
     {
+        Vector3 picPos0 = pictures[0].transform.position;
+        Vector3 picPos1 = pictures[1].transform.position;
+        Vector3 picPos2 = pictures[2].transform.position;
         if (direction == "left")
         {
             //Is left
-
+            pictures[2].transform.position = picPos0;
 
         }
         else if (direction == "right")
