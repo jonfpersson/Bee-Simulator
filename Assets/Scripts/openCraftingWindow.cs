9c69efebeddedf9214148c14382087851dc74437
Jonathan Persson <jonfpersson@gmail.com>
2018-06-08 16:01:53 +0200

    Fixed UI and finished crafting

---
+++ Assets/Scripts/openCraftingWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openCraftingWindow : MonoBehaviour {
+
+    public GameObject window;
+    bool isActivated;
+    // Use this for initialization
+	void Start () {
+        isActivated = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            isActivated = !isActivated;
+            Screen.lockCursor = !isActivated;
+            beeFly.craftingWindowIsActivated = isActivated;
+            window.SetActive(isActivated);
+
+        }
+    }
+}
